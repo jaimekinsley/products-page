6b0a65879754a2a7479fe6e0fa9aab8545f1ec4f
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-06 15:38:27 -0700

    completes step 1, creates a static list

---
+++ data/flowers.js
@@ -0,0 +1,10 @@
+const stargazer = {
+    id: 'stargazer',
+    name: 'Stargazer Lily',
+    image: 'stargazer.jpg',
+    description: 'Expressive both in beauty and size, lilies redefine elegance and purity. The Stargazer Lily is a large-bloomed bicolor variety with a dark pink strawberry blending into a soft vanilla exterior.  It is perfect for everything from formal events to arrangements for someone special.',
+    category: 'scented-flowers',
+    price: 4.50
+};
+
+export default flowers;
