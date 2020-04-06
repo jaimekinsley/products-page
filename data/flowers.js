d308984ec838495f9d8e44f380fede6b1dd61490
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-06 16:49:33 -0700

    finished step 3, made a Dom render function

--- data/flowers.js
+++ data/flowers.js
@@ -1,25 +1,25 @@
 const stargazer = {
     id: 'stargazer',
     name: 'Stargazer Lily',
-    image: 'stargazer.jpg',
+    image: '../assets/stargazer.jpg',
     description: 'Expressive both in beauty and size, lilies redefine elegance and purity. The Stargazer Lily is a large-bloomed bicolor variety with a dark pink strawberry blending into a soft vanilla exterior.  It is perfect for everything from formal events to arrangements for someone special.',
-    category: 'scented-flowers',
+    category: 'scented flowers',
     price: 4.50
 };
 
 const delphinium = {
     id: 'delphinium',
     name: 'Delphinium',
-    image: 'delphinium.jpg',
+    image: '../assets/delphinium.jpg',
     description: 'Derived from the Greek word “delphis,” these lush, dolphin-shaped flowers symbolize an open heart and ardent attachment and convey a feeling of lightness and levity.',
-    category: 'unscented-flowers',
+    category: 'unscented flowers',
     price: 2.00
 };
 
 const limonium = {
     id: 'limonium',
     name: 'Limonium',
-    image: 'limonium.jpg',
+    image: '../assets/limonium.jpg',
     description: 'Limonium is a filler flower characterized by small, paper-like blooms across multiple stems. They are frequently used in centerpieces and floral arrangements as a filler.',
     category: 'fillers',
     price: 1.00
@@ -28,25 +28,25 @@ const limonium = {
 const alstroemeria = {
     id: 'alstroemeria',
     name: 'Alstroemeria',
-    image: 'alstroemeria.jpg',
+    image: '../assets/alstroemeria.jpg',
     description: 'Known as the “Peruvian Lily,” these striped beauties come in all sorts of colors except true blue.  Symbolizing friendship and devotion, the alstroemeria’s leaves grow upside down, with the leaf twisting as it grows out from the stem, so that the bottom is facing upwards – much like the twists, turns and growth of our friendships.',
-    category: 'unscented-flowers',
+    category: 'unscented flowers',
     price: 2.00
 };
 
 const peony = {
     id: 'peony',
     name: 'Peony',
-    image: 'peony.jpg',
+    image: '../assets/peony.jpg',
     description: 'The traditional floral symbol of China, peonies are known as the flower of riches and honor.  With their lush, full, rounded bloom, peonies embody romance and prosperity and are regarded as an omen of good fortune and a happy marriage.  They are typically biannual in the floral world, appearing in the spring as larger and often darker blooms, and again in the fall as smaller and often smaller blooms.',
-    category: 'scented-flowers',
+    category: 'scented flowers',
     price: 3.00
 };
 
 const queenAnnesLace = {
     id: 'queenAnnesLace',
     name: 'Queen Anne\'s Lace',
-    image: 'queenAnnesLace.jpg',
+    image: '../assets/queenAnnesLace.jpg',
     description: 'Each stem of Queen Anne’s Lace has a large umbrella shaped head of clustered tiny, delicate blooms. Queen Anne’s Lace is a romantic flower on its own, or can be paired with other wildflowers for a natural look.',
     category: 'fillers',
     price: 1.00
@@ -55,10 +55,19 @@ const queenAnnesLace = {
 const callaLily = {
     id: 'callaLily',
     name: 'Calla Lily',
-    image: 'callaLily.jpg',
+    image: '../assets/callaLily.jpg',
     description: 'Though not really ‘lilies,’ Calla Lilies exude sheer elegance and simplicity with their long, straight stems and softly-twisted flowers. Their sleekness is perfect for weddings and formal events.',
-    category: 'unscented-flowers',
+    category: 'unscented flowers',
     price: 4.00
 };
+const flowers = [
+    stargazer,
+    delphinium,
+    limonium,
+    alstroemeria,
+    peony,
+    queenAnnesLace,
+    callaLily
+];
 
 export default flowers;
