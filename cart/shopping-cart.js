5c0182da40e8274d3b78e27bee736428c4b61a34
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-07 21:04:17 -0700

    completes second step 4, generating the shopping cart and makes it work

--- cart/shopping-cart.js
+++ cart/shopping-cart.js
@@ -1,14 +1,14 @@
 import cart from '../data/cart.js';
 import flowers from '../data/flowers.js';
-import { findById, calcLineTotal } from '../common/utils.js';
+import { findById } from '../common/utils.js';
+import { renderLineItem } from '../cart/render-line-items.js';
 
 
-const tbody = document.querySelector('tbody');
+const tbody = document.getElementById('checkout-table');
 
 for (let i = 0; i < cart.length; i++){
     const cartItem = cart[i];
     const flower = findById(flowers, cartItem.id);
-    const dom = calcLineTotal(cartItem, flower);
-
+    const dom = renderLineItem(cartItem, flower);
     tbody.appendChild(dom);
 }
