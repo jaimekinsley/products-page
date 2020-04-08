355327c7251855b2afb5559b5a7c048348e36c61
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-08 14:38:22 -0700

    makes the pop up alerts work

--- products/render-flowers.js
+++ products/render-flowers.js
@@ -1,3 +1,5 @@
+import { findById } from '../common/utils.js';
+
 function renderFlowers(flowers){
     const li = document.createElement('li');
     const h3 = document.createElement('h3');    
@@ -17,9 +19,49 @@ function renderFlowers(flowers){
     li.append(pCategory);
 
     const pPrice = document.createElement('p');
-    pPrice.textContent = flowers.price;
+    const flowerPrice = flowers.price;
+    const fixedPrice = `$${flowerPrice.toFixed(2)}`;
+    pPrice.textContent = fixedPrice; 
     li.append(pPrice);
 
+    const button = document.createElement('button');
+    button.textContent = 'Add';
+    button.value = flowers.id;
+    li.append(button);
+    
+    // add event listener to button
+    button.addEventListener('click', () => {
+        let json = localStorage.getItem('CART');
+        let cart;
+        if (json) {
+            cart = JSON.parse(json);
+        }
+        else {
+            cart = [];
+        }
+
+        let lineItem = findById(cart, flowers.id);
+
+        if (!lineItem) {
+            lineItem = {
+                id: flowers.id,
+                quantity:1
+            };
+
+            cart.push(lineItem);
+        }
+        else {
+            lineItem.quantity++;
+        }
+console.log(lineItem.quantity);
+
+        json = JSON.stringify(cart);
+        localStorage.setItem('CART', json);
+
+        alert('1 ' + flowers.name + ' added to cart');
+    });
+
+    
     return li;
 }
 
